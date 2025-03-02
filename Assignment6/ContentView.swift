de6c8d247a83c606c84a02537d1212ec084212a9
Anand Otiv <w0858857@myscc.ca>
2025-03-01 21:48:16 -0500

    Added the image, and started the text.

--- Assignment6/ContentView.swift
+++ Assignment6/ContentView.swift
@@ -10,11 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            
+            Image("Anand")
+            Text("Anand Otiv")
+                .font(.title)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .lineLimit(nil)
+            
+            Text("Program Of Study: Computer Programming")
+            Text("Year: 2026 \(Image(systemName: "graduationcap.fill"))")
+            Divider()
+            
+            
+
+//            Image(systemName: )
+           
+            
         }
+        
         .padding()
     }
 }
