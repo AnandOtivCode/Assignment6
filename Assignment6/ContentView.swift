e9a1832e45efd5b7b7d71109d7b5611fa3e85f85
Anand Otiv <w0858857@myscc.ca>
2025-03-01 22:18:05 -0500

    Finished a rough draft of the text, edited my photo to look more asthetically pleasing. Made a button to display my favourite course.

--- Assignment6/ContentView.swift
+++ Assignment6/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAlertShowing = false
+
     var body: some View {
         VStack {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundStyle(.tint)
-            
-            Image("Anand")
+            Spacer()
+            Image("Anand").resizable().aspectRatio(contentMode: .fit).frame(width: 300, height:300).clipShape(Circle())
+            Spacer()
             Text("Anand Otiv")
                 .font(.title)
                 .fontWeight(.heavy)
@@ -24,6 +28,17 @@ struct ContentView: View {
             Text("Program Of Study: Computer Programming")
             Text("Year: 2026 \(Image(systemName: "graduationcap.fill"))")
             Divider()
+            
+            Text("After I finish my diploma I'd like to work on machine learning and data science. I find data very interesting and I'd like to contribute a part to the Artificial Intelligence field of science some day.")
+            // computermouse.fill
+            
+            Button("What is my favourite course??", systemImage: "computermouse.fill"){
+                isAlertShowing.toggle()
+            }
+            
+            .alert("MAD103", isPresented: $isAlertShowing) {
+                //
+            }
             
             
 
